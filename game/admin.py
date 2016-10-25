f47c2492634ca20e0b2bbdfe31dba7041366d900
Matthieu Grandrie <matthieu.grandrie@lyra-network.com>
2016-10-25 10:14:19 +0200

    can add saisonscoring

--- game/admin.py
+++ game/admin.py
@@ -38,9 +38,6 @@ class SaisonScoringAdmin(InlineActionsModelAdminMixin, admin.ModelAdmin):
     actions = ['compute_scores_action']
     inlines = [JourneeScoringInline]
 
-    def has_add_permission(self, request):
-        return False
-
     def has_delete_permission(self, request, obj=None):
         return False
 
