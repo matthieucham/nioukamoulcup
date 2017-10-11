714dd4ed3a1f488ed86c62d399e45fc10347959f
mgrandrie <matthieu.grandrie@lyra-network.com>
2017-10-11 17:49:34 +0200

    slow slow

--- front/src/components/Formation.js
+++ front/src/components/Formation.js
@@ -2,17 +2,42 @@ import React, { Component } from 'react'
 import ReactSVG from 'react-svg'
 
 
-class Jersey extends Component {
+export class Jersey extends Component {
 
 	render() {
 		const svgPath = '/static/svg/'+this.props.club.maillot_svg+'.svg';
 		return (
-				<ReactSVG
-	    			path={ svgPath }
-	    			style={{ width:128, height:128, fill:this.props.club.maillot_color_bg, stroke:this.props.club.maillot_color1 }}
-	  			/>
-		);
+			<div className="jersey">
+			<ReactSVG
+			path={ svgPath }
+			style={{ width:64, height:64, fill:this.props.club.maillot_color_bg, stroke:this.props.club.maillot_color1 }}
+			/>
+			</div>
+			);
 	}
 }
 
-export default Jersey
+export class FieldPlayer extends Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div className="fieldPlayer">
+			<Jersey club={this.props.club} />
+			<h1>{ this.props.player.player.name }</h1>
+			<div className="details">
+			<h1>{ this.props.player.player.name }</h1>
+			<p>{ this.props.player.score }</p>
+			<p>{ this.props.club.nom }</p>
+			</div>
+			</div>
+			);
+	}
+
+
+}
+
+/* https://stackoverflow.com/questions/29913387/show-hide-components-in-reactjs */
