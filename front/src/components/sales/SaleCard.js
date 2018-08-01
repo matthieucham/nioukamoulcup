5ae864c2ca65b09cd68d44ec6bea067beeb784e0
Matthieu Grandrie <matthieu.grandrie@lyra-network.com>
2018-08-01 12:13:30 +0200

    SaleCard component finally !

---
+++ front/src/components/sales/SaleCard.js
@@ -0,0 +1,302 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Avatar from '@material-ui/core/Avatar';
+import Collapse from '@material-ui/core/Collapse';
+import ReactRevealText from 'react-reveal-text'
+
+
+
+class SaleBefore extends React.Component {
+	constructor(props) {
+		super(props)
+
+		this.state={
+			myOffer: this.findMyOffer(props.sale.auctions)
+		}
+
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	findMyOffer(auctions) {
+		for (const auction of auctions) {
+			if (auction.is_mine === true)
+				return ''+auction.value+' Ka';
+		}
+		return '-';
+	}
+
+	handleClick(e) {
+		e.preventDefault();
+		this.props.onViewSaleClicked()
+	}
+
+	render() {
+		const sale = this.props.sale;
+		return (
+			<div>
+				<div className="salecard-content">
+					<dl>
+						<dt>Auteur</dt>
+						<dd>{ sale.author.name }</dd>
+						<dt>Mise à prix</dt>
+						<dd>{ sale.min_price} Ka</dd>
+						<dt>Mon offre</dt>
+						<dd>{ this.state.myOffer}</dd>
+					</dl>
+					<div className="salecard-actions">
+						<Button variant="contained" color="primary" onClick={this.handleClick}>
+						Voir
+						</Button>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+
+class SaleDuring extends React.Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			auctionIndex: 0
+		}
+
+		this.handleNextClicked = this.handleNextClicked.bind(this)
+		this.handleRevealClicked = this.handleRevealClicked.bind(this)
+		this.handleResetClicked = this.handleResetClicked.bind(this)
+	}
+
+	handleNextClicked(e) {
+		e.preventDefault();
+		this.setState({ auctionIndex: this.state.auctionIndex + 1});
+	}
+
+	handleRevealClicked(e) {
+		e.preventDefault();
+		this.props.onRevealSaleClicked()
+	}
+
+	handleResetClicked(e) {
+		e.preventDefault();
+		this.props.onResetSaleClicked()
+	}
+
+	computeDiffWithCurrent(offers, auctionIndex) {
+		if (auctionIndex == 0) {
+			return '';
+		}
+		for (var i=auctionIndex-1; i>=0; i--) {
+			if (offers[i].is_valid) {
+				return '+'+(offers[auctionIndex].value - offers[i].value).toFixed(1);
+			}
+		}
+		return '';
+
+	}
+
+	render() {
+		const {sale}  = this.props;
+		let allOffers = sale.auctions.slice().reverse();
+		if (sale.type == 'PA' ) {
+			allOffers.push({value: sale.min_price, 'is_valid': true, 'is_mine':false});
+		}
+		allOffers = allOffers.reverse();
+
+		var {auctionIndex} = this.state;
+
+		if (auctionIndex >= allOffers.length) {
+			this.props.onLastOfferDone();
+			auctionIndex--;
+		}
+
+		const pastP = allOffers.slice(0, auctionIndex).map( (auction, index) => {
+			var classes = []
+			if (auction.is_mine) {
+				classes.push("ismine");
+			}
+			if (! auction.is_valid) {
+				classes.push("invalid");
+			}
+			return (<p key={'off'+index} className={classes}>{ auction.value }</p>);
+		})
+		return (
+			<div>
+				<div className="salecard-content">
+					<div className="past-offers">
+						{ pastP }
+					</div>
+					<div className="current-offer">
+						<h1>{ allOffers[auctionIndex].value } Ka</h1>
+						<p>{this.computeDiffWithCurrent(allOffers, auctionIndex)}</p>
+					</div>
+				</div>
+				<div className="salecard-actions">
+					<Button onClick={this.handleResetClicked}>Reset</Button>
+					<Button color="primary" onClick={this.handleRevealClicked}>Révéler</Button>
+					<Button variant="contained" color="primary" onClick={this.handleNextClicked}>Suivante</Button>
+				</div>
+			</div>
+		);
+	}
+
+}
+
+
+class SaleAfter extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state={
+			showWinner:false
+		}
+		this.handleResetClicked = this.handleResetClicked.bind(this)
+	}
+
+	componentDidMount() {
+		setTimeout(() => this.setState({showWinner: true}), 100)
+	}
+
+	handleResetClicked(e) {
+		e.preventDefault();
+		this.props.onResetSaleClicked()
+	}
+
+	computeDiff(sale) {
+		var offers = sale.auctions.filter(off => off.is_valid).reverse();
+		if (sale.type == 'PA') {
+			offers.push({value: sale.min_price});
+		}
+		if (offers.length == 1) {
+			return sale.amount.toFixed(1);
+		} else {
+			return (sale.amount - offers[1].value).toFixed(1);
+		}
+	}
+
+	render() {
+		const {sale} = this.props;
+		var hasWinner=false;
+		var winnerName;
+		var winnerAmount;
+		var winnerDiff;
+		if (sale.winner != null) {
+			hasWinner=true;
+			winnerName = sale.winner.name;
+			winnerAmount = sale.amount;
+			winnerDiff = this.computeDiff(sale);
+		} else {
+			if (sale.type == 'PA') {
+				hasWinner=true;
+				winnerName = sale.author.name;
+				winnerAmount = sale.min_price;
+			} else {
+				hasWinner=false;
+			}
+		}
+		return (
+			<div>
+			{hasWinner ? (
+				<div className="salecard-content">
+					<div className="winner">
+						<ReactRevealText show={this.state.showWinner} className="salecard-winner">{ winnerName }</ReactRevealText>
+					</div>
+					<div className="current-offer">
+						<h1>{winnerAmount+' Ka'}</h1>
+						{ winnerDiff && <p>{'écart: ' + winnerDiff+ 'Ka'}</p> }
+					</div>
+				</div>
+				) : (
+				<div className="salecard-content">
+					<h1>Aucune offre</h1>
+					<p>Vente annulée</p>
+				</div>
+				)}
+				
+				<div className="salecard-actions">
+					<Button onClick={this.handleResetClicked}>Reset</Button>
+				</div>
+			</div>
+		);
+	}
+}
+
+
+class SaleCardContent extends React.Component {
+	constructor(props) {
+		super(props)
+
+		this.state={
+			content: 'BEFORE'
+		}
+	}
+
+	switchContent(nextPanel) {
+		this.setState({content: nextPanel});
+	}
+
+	render() {
+		const {sale} = this.props;
+		const {content} = this.state;
+		return (
+			<CardContent>
+			{ (content == 'BEFORE') && 
+				<SaleBefore sale={sale} onViewSaleClicked={() => this.switchContent('DURING')}/>
+			}
+			{ (content == 'DURING') && 
+				<SaleDuring sale={sale} onLastOfferDone={() => this.switchContent('AFTER')} 
+										onResetSaleClicked={() => this.switchContent('BEFORE')}
+										onRevealSaleClicked={() => this.switchContent('AFTER')}/>
+			}
+			{ (content == 'AFTER') && 
+				<SaleAfter sale={sale} onResetSaleClicked={() => this.switchContent('BEFORE')}/>
+			}
+			</CardContent>
+		)
+	}
+}
+
+
+export class SaleCard extends React.Component {
+
+	constructor(props) {
+		super(props)
+
+		this.state={
+			expanded: false
+		}
+
+		this.handleExpandClick = this.handleExpandClick.bind(this)
+	}
+
+	handleExpandClick() {
+		this.setState(state => ({ expanded: !state.expanded }));
+	};
+
+	render() {
+		const sale = this.props.sale;
+		return (
+			<div>
+				<Card>
+					<CardHeader title={ sale.player.display_name } 
+								subheader={ sale.player.poste + ', ' + sale.player.club.nom }
+								avatar={
+									<Avatar>{ sale.type}</Avatar>			
+								}
+								onClick={this.handleExpandClick}
+					/>
+					<Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+						<SaleCardContent sale={sale} />
+					</Collapse>
+				</Card>
+			</div>
+			)
+	}
+}
--- front/src/pages/TestPage.js
+++ front/src/pages/TestPage.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-
-import LeagueRankingWidget from '../components/LeagueRanking';
+import { SaleCard } from '../components/sales/SaleCard'
 
 const mapStateToProps = state => {
 	return {
@@ -10,14 +9,71 @@ const mapStateToProps = state => {
 }
 
 class App extends Component {
+
+	
 	render() {
+		const SALE = {
+            "id": 163,
+            "rank": 1,
+            "type": "PA",
+            "player": {
+                "id": 149,
+                "url": "http://127.0.0.1:8000/game/home/stat/joueur/149/",
+                "prenom": "Karl",
+                "nom": "Toko Ekambi",
+                "surnom": "",
+                "display_name": "Karl Toko Ekambi",
+                "poste": "A",
+                "club": {
+                    "id": 11,
+                    "nom": "Angers",
+                    "maillot_svg": "jersey-stripes-v2",
+                    "maillot_color_bg": "#000000",
+                    "maillot_color_stroke": "#FFFFFF"
+                }
+            },
+            "author": {
+                "id": 15,
+                "url": "http://127.0.0.1:8000/game/league/1/ekyp/15",
+                "name": "The Gipsy Queens"
+            },
+            "min_price": 0.1,
+            "winner": {
+                "id": 15,
+                "url": "http://127.0.0.1:8000/game/league/1/ekyp/15",
+                "name": "The Gipsy Queens"
+            },
+            "amount": 8.0,
+            "auctions": [
+                {
+                    "value": 5.8,
+                    "is_valid": true,
+                    "is_mine": false
+                },
+                {
+                    "value": 6.1,
+                    "is_valid": false,
+                    "is_mine": false
+                },
+                {
+                    "value": 8.0,
+                    "is_valid": true,
+                    "is_mine": false
+                }
+            ]
+        }
+          ;
+
 		return (
 			<div className="react-app-inner">
 			<main>
-			
+				<div id="salesList">
+				<SaleCard sale={ SALE }/>
+				
+				</div>
 			</main>
 			<aside className="hg__right">
-			<LeagueRankingWidget ranking={ this.props.ranking }/>
+			
 			</aside>
 			</div>
 
