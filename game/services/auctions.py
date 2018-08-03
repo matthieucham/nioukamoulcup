d2165fdddf96d41aae7deae62d1c0414ce9f67fd
Matthieu Grandrie <matthieu.grandrie@lyra-network.com>
2018-08-03 15:54:49 +0200

    can_register_auction rule pour se débarrasser de la règle sur le nombre de PA

--- game/services/auctions.py
+++ game/services/auctions.py
@@ -54,76 +54,98 @@ def _make_signing_attr(sale):
                            'score_factor'] if 'score_factor' in sale.merkato_session.merkato.configuration else 1.0})
 
 
-    @transaction.atomic()
-    def solve_session(merkato_session):
-        assert merkato_session.merkato.mode == 'BID'
-        if merkato_session.solving > timezone.now():
-            raise SaleSolvingException('Merkato session not over yet, solving time is %s' % merkato_session.solving)
-        # first, apply releases
-        for release in transfer_models.Release.objects.filter(merkato_session=merkato_session):
-            league_models.BankAccount.objects.release(release)
-            release.signing.end = timezone.now()
-            release.done = True
-            release.signing.save()
-            release.save()
-        with locked_atomic_transaction.LockedAtomicTransaction(
-                league_models.BankAccount):  # prevents changes in BankAccount balance during resolution
-            # budgets and rosters are done, now solve.
-            sales = sorted(merkato_session.sale_set.all(), key=attrgetter('rank'))
-            # resolution must be sequential:
-            for s in sales:
-                solve_sale(s)
-        merkato_session.is_solved = True
-        merkato_session.save()
-        return merkato_session
+@transaction.atomic()
+def solve_session(merkato_session):
+    assert merkato_session.merkato.mode == 'BID'
+    if merkato_session.solving > timezone.now():
+        raise SaleSolvingException('Merkato session not over yet, solving time is %s' % merkato_session.solving)
+    # first, apply releases
+    for release in transfer_models.Release.objects.filter(merkato_session=merkato_session):
+        league_models.BankAccount.objects.release(release)
+        release.signing.end = timezone.now()
+        release.done = True
+        release.signing.save()
+        release.save()
+    with locked_atomic_transaction.LockedAtomicTransaction(
+            league_models.BankAccount):  # prevents changes in BankAccount balance during resolution
+        # budgets and rosters are done, now solve.
+        sales = sorted(merkato_session.sale_set.all(), key=attrgetter('rank'))
+        # resolution must be sequential:
+        for s in sales:
+            solve_sale(s)
+    merkato_session.is_solved = True
+    merkato_session.save()
+    return merkato_session
 
 
-    @transaction.atomic()
-    def solve_sale(sale):
-        auctions = sale.auctions.all()
-        processed_auctions = [_validate_auction(auc) for auc in auctions]
-        valid_auctions = [auc for auc in processed_auctions if auc.is_valid]
-        if not valid_auctions:
-            _handle_no_auction(sale)
-        else:
-            winner = _find_winner(valid_auctions)
-            _set_winning_auction(winner)
-        return sale
+@transaction.atomic()
+def solve_sale(sale):
+    auctions = sale.auctions.all()
+    processed_auctions = [_validate_auction(auc) for auc in auctions]
+    valid_auctions = [auc for auc in processed_auctions if auc.is_valid]
+    if not valid_auctions:
+        _handle_no_auction(sale)
+    else:
+        winner = _find_winner(valid_auctions)
+        _set_winning_auction(winner)
+    return sale
 
 
-    def _set_winning_auction(auction):
-        auction.sale.winning_auction = auction
-        auction.sale.save()
+def _set_winning_auction(auction):
+    auction.sale.winning_auction = auction
+    auction.sale.save()
 
 
-    def _find_winner(processed_auctions):
-        # sort by highest vaalue
-        sorted_auctions = sorted(processed_auctions, key=lambda a: a.value, reverse=True)
-        max_val = sorted_auctions[0].value
-        possible_winners = [auc for auc in sorted_auctions if auc.value == max_val]
-        # pick one in possible_winners
-        if len(possible_winners) == 1:
-            return possible_winners[0]
-        else:
-            secure_random = random.SystemRandom()
-            winner = secure_random.choice(possible_winners)
-            return winner
+def _find_winner(processed_auctions):
+    # sort by highest vaalue
+    sorted_auctions = sorted(processed_auctions, key=lambda a: a.value, reverse=True)
+    max_val = sorted_auctions[0].value
+    possible_winners = [auc for auc in sorted_auctions if auc.value == max_val]
+    # pick one in possible_winners
+    if len(possible_winners) == 1:
+        return possible_winners[0]
+    else:
+        secure_random = random.SystemRandom()
+        winner = secure_random.choice(possible_winners)
+        return winner
 
 
-    def _handle_no_auction(sale):
-        sale.winning_auction = None
-        sale.save()
+def _handle_no_auction(sale):
+    sale.winning_auction = None
+    sale.save()
 
 
-    @transaction.atomic()
-    def _validate_auction(auction):
-        try:
-            auction.validate()
-            auction.is_valid = True
-        except transfer_models.Auction.AuctionNotValidException as e:
-            auction.is_valid = False
-            auction.reject_cause = e.code
-        auction.save()
-        return auction
+@transaction.atomic()
+def _validate_auction(auction):
+    try:
+        auction.validate()
+        auction.is_valid = True
+    except transfer_models.Auction.AuctionNotValidException as e:
+        auction.is_valid = False
+        auction.reject_cause = e.code
+    auction.save()
+    return auction
 
 
+def can_register_auction(team, merkato):
+    """
+    Vérifie que cette ékyp est autorisée à envoyer des enchères. La règle:
+    - avoir une PA en cours
+    ou bien
+    - en être dans les 3 premières sessions d'un merkato
+    ou bien
+    - avoir déjà posté (N/2 -1) PA où N est le nombre de sessions écoulées
+    :return: True |False
+    """
+    expired_sessions = merkato.merkatosession_set.filter(solving__lt=timezone.now()).count()
+    if expired_sessions < 4:
+        return True
+    current_pa = transfer_models.Sale.objects.filter(merkato_session__merkato=merkato,
+                                                     merkato_session__solving__gt=timezone.now(),
+                                                     team=team).count()
+    if current_pa > 0:
+        return True
+    previous_pas = transfer_models.Sale.objects.filter(merkato_session__merkato=merkato,
+                                                       merkato_session__solving__lt=timezone.now(),
+                                                       team=team).count()
+    return (expired_sessions // 2) - 1 <= previous_pas
