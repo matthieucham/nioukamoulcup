b664d75dbd58aa79467222c1280fff9c6c3e0822
Matthieu Grandrie <matthieu.grandrie@lyra-network.com>
2018-09-07 16:19:27 +0200

    inside a draggable list

--- front/src/pages/TestPage.js
+++ front/src/pages/TestPage.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import {
+  SortableContainer,
+  SortableElement,
+  arrayMove
+} from "react-sortable-hoc";
 import PlayerPicker from "../components/sales/PlayerPicker";
 
 /* const mapStateToProps = state => {
@@ -8,14 +13,25 @@ import PlayerPicker from "../components/sales/PlayerPicker";
   };
 }; */
 
+const SortableItem = SortableElement(({ value }) => <li>{value}</li>);
+
+const SortableList = SortableContainer(({ items }) => {
+  return (
+    <ul>
+      {items.map((value, index) => (
+        <SortableItem key={`item-${index}`} index={index} value={value} />
+      ))}
+    </ul>
+  );
+});
+
 export class TestPage extends Component {
   render() {
     return (
       <div className="react-app-inner">
         <main>
           <article id="home-main">
-            <PlayerPicker />
-            
+          <SortableList items={[<PlayerPicker key="pp1" />, <PlayerPicker key="pp2" />]} />
           </article>
         </main>
         <aside className="hg__right" />
