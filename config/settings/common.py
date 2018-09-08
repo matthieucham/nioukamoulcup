a00d163ce7784e6f0441f5a9e28f1deedab34225
matthieucham <matthieu.cham@gmail.com>
2018-09-08 21:53:23 +0200

    deactivate middleware for now

--- config/settings/common.py
+++ config/settings/common.py
@@ -92,7 +92,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'game.middleware.ActiveLeagueMiddleware',
+    # 'game.middleware.ActiveLeagueMiddleware',
 ]
 
 # DEBUG
