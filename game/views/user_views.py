2c70a2ed6ee1897beb34313702f5c339050536ae
matthieucham <matthieu.cham@gmail.com>
2018-09-20 22:09:35 +0200

    wip slow

--- game/views/user_views.py
+++ game/views/user_views.py
@@ -1,12 +1,17 @@
 from django.views.generic import TemplateView, ListView
 from game.models.league_models import Team
+from django.db.models import Count, Q
 
 
 class TeamListView(ListView):
-
     template_name = 'game/user/team_list.html'
 
     def get_queryset(self):
-        return Team.objects.filter(managers__user=self.request.user)
+        return Team.objects.filter(managers__user=self.request.user).annotate(
+            is_captain=Count(1, filter=Q(managers__is_team_captain=True)))
 
     context_object_name = 'teams'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(TeamListView, self).get_context_data(*args, **kwargs)
+        return context
