8774380f790adb11de0915cd3a1c6105a828c54e
Matthieu Grandrie <grandriem@mfi.fr>
2018-10-03 15:33:53 +0200

    more admin

--- game/admin_import.py
+++ game/admin_import.py
@@ -153,5 +153,12 @@ class LeagueAdmin(InlineActionsModelAdminMixin, admin.ModelAdmin):
     inlines = [LeagueDivisionInline, TeamInline, LeagueInvitationInline, ]
 
 
+class TeamAdmin(admin.ModelAdmin):
+    model = models.Team
+    list_display = ('name', 'league', 'division', 'get_managers_names')
+
+
 admin.site.register(Entry, EntryLeagueAdmin)
-admin.site.register(models.League, LeagueAdmin)
+admin_import_site.register(models.League, LeagueAdmin)
+admin_import_site.register(models.LeagueInstance)
+admin_import_site.register(models.Team, TeamAdmin)
