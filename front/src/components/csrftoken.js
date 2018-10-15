e450cc64986cb481b05464ff4742f1a1bef82c45
Matthieu Grandrie <grandriem@mfi.fr>
2018-10-15 11:02:16 +0200

    debug csrf

--- front/src/components/csrftoken.js
+++ front/src/components/csrftoken.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Cookies from "js-cookie";
 
+console.log('CSRF debug');
+console.log(Cookies.get())
+
 var csrftoken = Cookies.get("csrftoken");
 
 const CSRFToken = () => {
