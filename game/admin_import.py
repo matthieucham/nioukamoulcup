704f47b975123505b91d4d0e2d905221c0285e5c
Matthieu Grandrie <matthieu.grandrie@stack-labs.com>
2020-10-20 13:43:52 +0200

    signing admin

--- game/admin_import.py
+++ game/admin_import.py
@@ -216,9 +216,15 @@ class TeamAdmin(admin.ModelAdmin):
     model = models.Team
     list_display = ('name', 'league', 'division', 'status', 'get_managers_names')
 
+class SigningAdmin(admin.ModelAdmin):
+    model = models.Signing
+    list_display = ('player', 'league_instance', 'team', 'begin', 'end')
+
+
 
 admin.site.register(Entry, EntryLeagueAdmin)
 admin_import_site.register(models.League, LeagueAdmin)
 admin_import_site.register(models.LeagueInstance)
 admin_import_site.register(models.LeagueInstancePhase)
 admin_import_site.register(models.Team, TeamAdmin)
+admin_import_site.register(models.Signing, SigningAdmin)
